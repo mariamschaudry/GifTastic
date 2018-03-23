61ab097cf2420c240c7ce69809c894cc36896319
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-03-23 17:19:09 -0400

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+// Psuedo Code //
+    // Create an array of the gif topics 
+    // Create a button for each gif topic 
+    // Use a for loop that appends a button to each string in array
+    // User should grab 10 static, non-animated gifs from GIPHY API
+    // When user should click still GIPHY image, it should animate (pause/play)
+    // Under each gif, display it's rating (PG, G, etc.)
+    // Create a form for the page that takes user input and adds it to topics array
+    // Make a function call that takes each topic in array and remakes the buttons on page
+
+// API KEY: MK0cvZOOOfzvXyxD6dKC4HumUEx6jLUE //
+
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=api_key=MK0cvZOOOfzvXyxD6dKC4HumUEx6jLUe"
+
+// =========================================================================================================== //
+
+
+// Topics array //
+
+var netflixShows = ["Shameless", "Girl Boss", "Narcos", "New Girl", "Santa Clarita Diet"];
+
+// Creating buttons from the array //
+
+
+
+
+
